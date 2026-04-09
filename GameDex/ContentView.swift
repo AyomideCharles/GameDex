0461868621162352328ccca0c7410a8e51e7fbbe
AyomideCharles <dettyboy@icloud.com>
2026-04-09 08:30:15 +0100

    Initial Commit

---
+++ GameDex/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  GameDex
+//
+//  Created by Charles on 4/9/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ GameDex/GameDexApp.swift
@@ -0,0 +1,17 @@
+//
+//  GameDexApp.swift
+//  GameDex
+//
+//  Created by Charles on 4/9/26.
+//
+
+import SwiftUI
+
+@main
+struct GameDexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
